1eff90ccde47ee55bec0b656e881b5720040e26e
themdshakibul <mdshakibul88240@gmail.com>
2026-01-29 14:42:34 +0600

    Task 3 compldted

--- Task03.js
+++ Task03.js
@@ -7,4 +7,30 @@
 // `'TomTimTinTik'`
 
 // **Input:**
-var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+var numbers = ["Tom", "Tim", "Tin", "Tik"];
+
+// Basic and clean Method 2
+let resul = "";
+
+for (const word of numbers) {
+  resul += word;
+}
+console.log(resul);
+
+// Method three
+let combainedString = "";
+
+for (const item of numbers) {
+  combainedString += item;
+}
+console.log(combainedString);
+
+// for of condition Method 2
+let result = "";
+
+for (const item of numbers) {
+  if (item.length > 0) {
+    result += item;
+  }
+}
+console.log(result);
